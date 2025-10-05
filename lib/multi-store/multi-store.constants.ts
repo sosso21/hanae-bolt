44469e9b2f70a79ef0de647cbdd66751e026cd09
sosso21 <sofianetop21.st@gmail.com>
2025-10-05 09:54:06 +0100

    ✨ (add): restore backup files for out-street-2

--- lib/multi-store/multi-store.constants.ts
+++ lib/multi-store/multi-store.constants.ts
@@ -28,6 +28,17 @@ export const STORES: Store[] = [
     NEXT_SHOPIFY_STORE_DOMAIN:
       process.env.NEXT_CHAMPION_STREET_SHOPIFY_STORE_DOMAIN ?? "",
   },
+  {
+    STORE_ID: "3",
+    STORE_KIND: "REAL",
+    NEXT_SHOPIFY_ACCESS_TOKEN:
+      process.env.NEXT_OUT_STREET_SHOPIFY_ACCESS_TOKEN ?? "",
+    NEXT_SHOPIFY_API_KEY: process.env.NEXT_OUT_STREET_SHOPIFY_API_KEY ?? "",
+    NEXT_SHOPIFY_SECRET_KEY:
+      process.env.NEXT_OUT_STREET_SHOPIFY_SECRET_KEY ?? "",
+    NEXT_SHOPIFY_STORE_DOMAIN:
+      process.env.NEXT_OUT_STREET_SHOPIFY_STORE_DOMAIN ?? "",
+  },
 ];
 
 export function getStore(storeId: string) {
